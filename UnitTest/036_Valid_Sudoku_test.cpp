8544e6431d5d709a0435f46dfb819092a139f34a
pangdahai1994 <zhuhaichao_518@126.com>
2017-07-19 11:44:40 +0800

    036finish

--- UnitTest/036_Valid_Sudoku_test.cpp
+++ UnitTest/036_Valid_Sudoku_test.cpp
@@ -6,13 +6,25 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace My036_Valid_Sudoku_UnitTest
 {		
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(ValidSudokuTest)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(isValidSudokuTest)
 		{
-			// TODO: 在此输入测试代码
+			Solution solution;
+			string arr[] = { ".87654321", "2........", "3........", "4........", "5........", "6........", "7........", "8........", "9........" };
+			vector<vector<char>> board;
+			for (int i = 0; i<9; i++) {
+				vector<char> temp;
+				for (int j = 0; j < 9; j++) {
+					temp.push_back(arr[i][j]);
+				}
+				board.push_back(temp);
+			}
+			Assert::AreEqual(solution.isValidSudoku(board),true);
+			Logger::WriteMessage("testDivide() finish testing!\n");
+			system("pause");
 		}
 
 	};
